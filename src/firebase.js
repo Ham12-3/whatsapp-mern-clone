f778b31bb990715dccff9fbfcf4eba7e7bdca7c3
Abdulhamid <mobolaji2309@gmail.com>
2023-10-02 07:27:27 +0100

    updaitng

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDug-dEB-Zl-AyHQcsAFuBm5YNa-SIa8Xo",
+  authDomain: "whatsapp-mern-44959.firebaseapp.com",
+  projectId: "whatsapp-mern-44959",
+  storageBucket: "whatsapp-mern-44959.appspot.com",
+  messagingSenderId: "458536290302",
+  appId: "1:458536290302:web:d31b151d3799adbb3c62af",
+};
