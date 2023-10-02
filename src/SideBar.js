ae38bcffdd1ba3d5ff0146be0c956ec2b75c8823
Abdulhamid <mobolaji2309@gmail.com>
2023-10-02 08:47:09 +0100

    updating

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,9 +1,35 @@
 import React from "react";
 import "./SideBar.css";
+import {
+  Chat,
+  DonutLarge,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
 const SideBar = () => {
   return (
-    <div>
-      <h1>I am the sidebar</h1>
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
     </div>
   );
 };
