31d1cd17fff46b704267621c9d5851f248957f26
Abdulhamid <mobolaji2309@gmail.com>
2023-10-02 09:52:55 +0100

    updating

--- src/SideBar.js
+++ src/SideBar.js
@@ -6,6 +6,7 @@ import {
   MoreVert,
   SearchOutlined,
 } from "@mui/icons-material";
+import SidebarChat from "./SidebarChat";
 import { Avatar, IconButton } from "@mui/material";
 const SideBar = () => {
   return (
@@ -29,6 +30,12 @@ const SideBar = () => {
           <SearchOutlined />
           <input type="text" placeholder="Search or start new chat" />
         </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
