42948a511feea4b8a39024545bae95fbad036353
Abdulhamid <mobolaji2309@gmail.com>
2023-10-02 14:28:40 +0100

    updating

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,74 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@mui/material";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
 const Chat = () => {
   return (
     <div className="chat">
-      <h1>I am the chat</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at....</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Abdulhamid</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Abdulhamid</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Abdulhamid</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Abdulhamid</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <div className="chat__footer">
+          <InsertEmoticon />
+          <form action="">
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              placeholder="Type a message"
+            />
+            <button onClick={sendMessage} type="submit">
+              {" "}
+              Send a message
+            </button>
+          </form>
+          <Mic />
+        </div>
+      </div>
     </div>
   );
 };
