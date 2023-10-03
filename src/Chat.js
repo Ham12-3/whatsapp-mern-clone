dff4e7a27e208b479a52b7392bbac5befa6bc1b0
Abdulhamid <mobolaji2309@gmail.com>
2023-10-03 09:47:49 +0100

    update

--- src/Chat.js
+++ src/Chat.js
@@ -32,10 +32,12 @@ const Chat = ({ messages }) => {
 
       <div className="chat__body">
         {messages.map((message) => (
-          <p className="chat__message">
-            <span className="chat__name">Abdulhamid</span>
-            This is a message
-            <span className="chat__timestamp">{new Date().toUTCString()}</span>
+          <p
+            className={`chat__message ${message.received && "chat__receiver"}`}
+          >
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">{message.timestamp}</span>
           </p>
         ))}
 
